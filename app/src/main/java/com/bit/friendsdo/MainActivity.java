604c26817a840a902917343eaec8ec268dc27a52
Abdelfatah <bnb.fatah@gmail.com>
2023-08-18 20:19:51 +0100

    FireStore: add new task

--- app/src/main/java/com/bit/friendsdo/MainActivity.java
+++ app/src/main/java/com/bit/friendsdo/MainActivity.java
@@ -3,7 +3,14 @@ package com.bit.friendsdo;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -14,10 +21,19 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SharedPreferences sharedPreferences;
+    private static final String PREF_NAME = "MyPref";
+    private static final String KEY_NAME = "user_name";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sharedPreferences = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+
+        if (!sharedPreferences.contains(KEY_NAME)) {
+            showNameDialog();
+        }
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.nav_bar);
 
@@ -34,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
                 selectedFragment = new TaskListFragment();
             } else if (item.getItemId() == R.id.nav_task_done) {
                 selectedFragment = new TaskDoneListFragment();
+            } else if(item.getItemId() == R.id.nav_add_task) {
+                selectedFragment  = new NewTaskFragment();
             }
 
             getSupportFragmentManager().beginTransaction()
@@ -42,12 +60,36 @@ public class MainActivity extends AppCompatActivity {
 
             return true;
         });
+    }
 
-//        FirebaseFirestore db = FirebaseFirestore.getInstance();
-//        CollectionReference friendsTaskCollection = db.collection("FriendTask");
-//
-//        FriendTask task = new FriendTask("clean the room", new Date(), "Fatah", false, null);
-//        friendsTaskCollection.add(task);
-//        Toast.makeText(this, "task added", Toast.LENGTH_SHORT).show();
+    private void showNameDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_enter_name, null);
+        builder.setView(dialogView);
+
+        final EditText editTextName = dialogView.findViewById(R.id.editTextName);
+        Button buttonSave = dialogView.findViewById(R.id.buttonSave);
+
+        final AlertDialog dialog = builder.create();
+        dialog.setCancelable(false); // Prevent dismissing the dialog by clicking outside
+
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editTextName.getText().toString().trim();
+                if (!name.isEmpty()) {
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString(KEY_NAME, name);
+                    editor.apply();
+                    dialog.dismiss();
+                } else {
+                    // Show an error message indicating that the name is required
+                    editTextName.setError("Name is required");
+                }
+            }
+        });
+
+        dialog.show();
     }
 }
---
+++ app/src/main/java/com/bit/friendsdo/NewTaskFragment.java
@@ -0,0 +1,62 @@
+package com.bit.friendsdo;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Date;
+
+public class NewTaskFragment extends Fragment {
+    private SharedPreferences sharedPreferences;
+    private static final String PREF_NAME = "MyPref";
+    private static final String KEY_NAME = "user_name";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.new_task_fragment, container, false);
+
+        EditText taskEditText = rootView.findViewById(R.id.task_edit_text_view);
+        Button publishBtn = rootView.findViewById(R.id.publish_btn);
+
+        publishBtn.setOnClickListener(v -> {
+            if (taskEditText.getText().toString().trim().isEmpty()) {
+                taskEditText.setError("Required");
+            } else {
+                sharedPreferences = getActivity().getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+                String name = sharedPreferences.getString(KEY_NAME, "");
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                CollectionReference friendsTaskCollection = db.collection("FriendTask");
+
+                FriendTask task = new FriendTask(taskEditText.getText().toString().trim(), new Date(), name, false, null);
+                friendsTaskCollection.add(task);
+                Toast.makeText(getContext(), "task added", Toast.LENGTH_SHORT).show();
+                taskEditText.setText("");
+//                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//                TaskListFragment targetFragment = new TaskListFragment();
+//                fragmentTransaction.replace(R.id.frame, targetFragment);
+//                fragmentTransaction.commit();
+            }
+        });
+        return rootView;
+    }
+}
