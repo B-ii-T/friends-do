cbf5d0ea058483bce5d120ed4a1249db44689cf6
Abdelfatah <bnb.fatah@gmail.com>
2023-08-19 16:27:30 +0100

    User Interface: improvements (splash, icon)

--- app/src/main/java/com/bit/friendsdo/SplashScreen.java
+++ app/src/main/java/com/bit/friendsdo/SplashScreen.java
@@ -3,8 +3,11 @@ package com.bit.friendsdo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
 
@@ -16,6 +19,8 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
 
         LottieAnimationView animationView = findViewById(R.id.animated_logo);
+        TextView textView = findViewById(R.id.label_textview);
+
         animationView.addAnimatorListener(new Animator.AnimatorListener() {
             @Override
             public void onAnimationStart(Animator animator) {
@@ -40,5 +45,33 @@ public class SplashScreen extends AppCompatActivity {
                 // Animation repeated
             }
         });
+
+        // Create an ObjectAnimator to fade in the TextView
+        ObjectAnimator fadeInAnimator = ObjectAnimator.ofFloat(textView, "alpha", 0f, 1f);
+        fadeInAnimator.setDuration(1000); // Set the duration of the fade-in animation
+        fadeInAnimator.setStartDelay(500); // Set a delay before the animation starts
+        fadeInAnimator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+                // Make the TextView visible when the animation starts
+                textView.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                // Animation ended, no additional action needed
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+                // Animation canceled, no additional action needed
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+                // Animation repeated, no additional action needed
+            }
+        });
+        fadeInAnimator.start(); // Start the animation
     }
 }
