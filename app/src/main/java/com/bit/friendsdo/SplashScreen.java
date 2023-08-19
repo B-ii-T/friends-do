d972411ab33c32e2200415d998be443f25110348
Abdelfatah <bnb.fatah@gmail.com>
2023-08-19 14:11:58 +0100

    User Interface: splash screen

---
+++ app/src/main/java/com/bit/friendsdo/SplashScreen.java
@@ -0,0 +1,44 @@
+package com.bit.friendsdo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.Animator;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        LottieAnimationView animationView = findViewById(R.id.animated_logo);
+        animationView.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+                // Animation started
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                // Animation ended, navigate to the next screen
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // Optional: Close the current activity if needed
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+                // Animation canceled
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+                // Animation repeated
+            }
+        });
+    }
+}
